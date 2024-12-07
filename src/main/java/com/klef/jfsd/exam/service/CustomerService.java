7fe1ef1d1f1c1804bbc8b9ac06efb5e1d6458424
Asish-Praneeth-N <asishpraneethnamala@gmail.com>
2024-12-07 11:01:03 +0530

    Initial commit

---
+++ src/main/java/com/klef/jfsd/exam/service/CustomerService.java
@@ -0,0 +1,25 @@
+package com.klef.jfsd.exam.service;
+
+import com.klef.jfsd.exam.model.Customer;
+import com.klef.jfsd.exam.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Customer updateCustomer(Long id, String name, String address) {
+        Optional<Customer> customerOpt = customerRepository.findById(id);
+        if (customerOpt.isPresent()) {
+            Customer customer = customerOpt.get();
+            customer.setName(name);
+            customer.setAddress(address);
+            return customerRepository.save(customer);
+        }
+        throw new RuntimeException("Customer not found with ID: " + id);
+    }
+}
